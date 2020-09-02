9174bd8871729d708fd79e7828c86f7e46a3e1b3
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2020-09-02 09:40:36 -0400

    update-automated-save

--- pkg/graphQL/tags.go
+++ pkg/graphQL/tags.go
@@ -1,27 +1,48 @@
 package graphQL
 
 import (
-	"fmt"
 	"github.com/mchirico/agil/pkg/qtypes"
 	"regexp"
+	"time"
 )
 
-func FindTags(q qtypes.Q) {
+type Tags struct {
+	Note     string
+	ColID    string
+	NodeID    string
+	Tag       []byte
+	UpdatedAt time.Time
+	CreatedAt time.Time
+	URL       string
+	IsArchived bool
+}
+
+func FindTags(q qtypes.Q) []Tags {
+	tags := []Tags{}
 	re := regexp.MustCompile(`:[a-z|A-Z].*$`)
-	fmt.Printf("\n  Columns:\n")
-	for i, column := range q.Repository.Projects.Edges[0].Node.Columns.Edges {
-		if 1==2 {
-			fmt.Printf("i:%v v: %v updated: %v      id: %v\n", i,
-				column.Node.Name, column.Node.UpdatedAt, column.Node.Id)
-		}
+
+	for _, column := range q.Repository.Projects.Edges[0].Node.Columns.Edges {
+
 		for _, card := range column.Node.Cards.Edges {
-			//fmt.Printf("%v\n",card.Node.Note)
+
 			s := card.Node.Note
 			ok := re.Find([]byte(s))
 			if ok != nil {
-				fmt.Printf("Found: %s\n",ok)
+				tag := Tags{}
+				tag.Note = card.Node.Note
+				tag.NodeID = card.Node.Id
+				tag.URL = card.Node.Url
+				tag.UpdatedAt = card.Node.UpdatedAt
+				tag.CreatedAt = card.Node.CreatedAt
+				tag.IsArchived = card.Node.IsArchived
+				tag.ColID = column.Node.Id
+				tag.Tag = ok
+
+
+				tags = append(tags,tag)
+
 			}
 		}
 	}
-
+	return tags
 }
--- pkg/graphQL/tags_test.go
+++ pkg/graphQL/tags_test.go
@@ -9,6 +9,14 @@ func Test_FindTags(t *testing.T) {
 
 	q := testing_graphql.MockQueryGraphQL(t)
 	//fmt.Println(q.Repository.Projects.Edges[0].Node.Columns.Edges[0].Node.Name)
-	FindTags(q)
-
+	r := FindTags(q)
+	found := false
+	for _,v := range r {
+		if string(v.Tag) == ":agil:testing" {
+			found = true
+		}
+	}
+	if !found {
+		t.Fatalf("Tag \"agil:testing\" not found\n")
+	}
 }
