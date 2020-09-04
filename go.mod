1db40435347af8c07b269f09a4ea64c7433d4e32
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2020-09-03 22:44:56 -0400

    update-automated-save

--- go.mod
+++ go.mod
@@ -5,7 +5,6 @@ go 1.15
 require (
 	github.com/ajstarks/svgo v0.0.0-20200725142600-7a3c8b57fecb
 	github.com/kennygrant/sanitize v1.2.4
-	github.com/mchirico/date v0.0.0-20190619124519-ced294d6068a // indirect
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/shurcooL/githubv4 v0.0.0-20200802174311-f27d2ca7f6d5
 	github.com/shurcooL/graphql v0.0.0-20181231061246-d48a9a75455f // indirect
--- pkg/http/handles/handles_test.go
+++ pkg/http/handles/handles_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -37,6 +38,42 @@ func Test_TimeStampIt(t *testing.T) {
 
 	if !result {
 		t.Fatalf("Couldn't swap new function")
+	}
+
+}
+
+func Test_RootGET(t *testing.T) {
+	req := httptest.NewRequest("GET", "/", nil)
+	w := httptest.NewRecorder()
+	BaseRoot(w, req)
+
+	resp := w.Result()
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	fmt.Println(resp.StatusCode)
+	fmt.Println(resp.Header.Get("Content-Type"))
+	fmt.Println(string(body))
+
+	if !strings.Contains(string(body), "version:") {
+		t.Fatalf("GET on root failed")
+	}
+
+}
+
+func Test_RootPUT(t *testing.T) {
+	req := httptest.NewRequest("PUT", "/", nil)
+	w := httptest.NewRecorder()
+	BaseRoot(w, req)
+
+	resp := w.Result()
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	fmt.Println(resp.StatusCode)
+	fmt.Println(resp.Header.Get("Content-Type"))
+	fmt.Println(string(body))
+
+	if resp.StatusCode != 200 {
+		t.Fatalf("PUT is causing error")
 	}
 
 }
